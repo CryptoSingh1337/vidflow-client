d0eeb623d61ce2393c179e2f19b9d52a251ddbcd
Saransh Kumar <cryptosingh@gmail.com>
2021-10-03 16:05:46 +0530

    update description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,16 @@ export default {
   target: 'static',
   head: {
     title: 'VidFlow',
+    generate: {
+      fallback: true
+    },
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'VidFlow is a video streaming platform' },
+      { hid: 'description', name: 'description', content: 'VidFlow - A youtube clone' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/v.png' }
