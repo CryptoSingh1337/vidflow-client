fbec8fa6d2840c01e77a89e0766303665754b8de
Saransh Kumar <cryptosingh@gmail.com>
2021-10-04 23:41:38 +0530

    updated bootstrap thumbnail resolutions

--- assets/data/videos.js
+++ assets/data/videos.js
@@ -2,7 +2,7 @@ const data = [
 	{
 		"id": 1,
 		"title": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?technology",
+		"thumbnail": "https://source.unsplash.com/1280x720/?technology",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "1.1M",
@@ -11,7 +11,7 @@ const data = [
 	{
 		"id": 2,
 		"title": "It is a long established fact that a reader will be distracted by the readable",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?gaming",
+		"thumbnail": "https://source.unsplash.com/1280x720/?gaming",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "89K",
@@ -20,7 +20,7 @@ const data = [
 	{
 		"id": 3,
 		"title": "It look like readable English. Many desktop publishing packages and web page editors",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?personal",
+		"thumbnail": "https://source.unsplash.com/1280x720/?personal",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "65K",
@@ -29,7 +29,7 @@ const data = [
 	{
 		"id": 4,
 		"title": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?food",
+		"thumbnail": "https://source.unsplash.com/1280x720/?food",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "450K",
@@ -38,7 +38,7 @@ const data = [
 	{
 		"id": 5,
 		"title": "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?food",
+		"thumbnail": "https://source.unsplash.com/1280x720/?food",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "9K",
@@ -47,7 +47,7 @@ const data = [
 	{
 		"id": 6,
 		"title": "If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?news",
+		"thumbnail": "https://source.unsplash.com/1280x720/?news",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "10K",
@@ -56,7 +56,7 @@ const data = [
 	{
 		"id": 7,
 		"title": "Lorem Ipsum comes from sections 1.10.32",
-		"thumbnail": "https://source.unsplash.com/1920x1080/?fashion",
+		"thumbnail": "https://source.unsplash.com/1280x720/?fashion",
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "12K",
