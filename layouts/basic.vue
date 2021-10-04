5e9f543cc3e41f583e4c69950257b226fb1a8ff3
Saransh Kumar <cryptosingh@gmail.com>
2021-10-05 00:30:44 +0530

    created error page

--- layouts/basic.vue
+++ layouts/basic.vue
@@ -1,11 +1,11 @@
 <template>
-  <h1>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor recusandae
-    distinctio consequatur? Quam eius eligendi nobis iste. Est non temporibus
-    neque atque, esse optio culpa officiis a impedit. Cumque, ipsam?
-  </h1>
+  <v-app>
+    <Nuxt />
+  </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "basic",
+};
 </script>
