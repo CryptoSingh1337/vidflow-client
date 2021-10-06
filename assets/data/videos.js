eca05c17a3d5368e36de288cf339f96ef68e91e1
Saransh Kumar <cryptosingh@gmail.com>
2021-10-06 18:53:54 +0530

    added long views for video having id 7

--- assets/data/videos.js
+++ assets/data/videos.js
@@ -72,6 +72,7 @@ const data = [
 		"channelName": "Channel Name",
 		"channelLink": "/channel/Cryptosingh",
 		"views": "12K",
+		"longViews": "12587",
 		"time": "10 hours ago",
 		"description": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary"
 	}
