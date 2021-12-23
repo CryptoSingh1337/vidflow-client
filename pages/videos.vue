410d105703d60b0ebb69956388c4654cc7089a1d
Saransh Kumar <cryptosingh@gmail.com>
2021-12-23 19:26:05 +0530

    Closes #9: Created Your videos page.

---
+++ pages/videos.vue
@@ -0,0 +1,30 @@
+<template>
+  <Unauthorize
+    v-if="!$auth.loggedIn"
+    :icon="icon"
+    :heading="heading"
+    :caption="caption"
+  />
+</template>
+
+<script>
+import Unauthorize from "@/components/UnauthorizedUser/Unauthorize.vue";
+
+export default {
+  head() {
+    return {
+      title: "Your vidoes - VidFlow",
+    };
+  },
+  components: {
+    Unauthorize,
+  },
+  data() {
+    return {
+      icon: "mdi-video",
+      heading: "Keep your eyes on your vidoes stats",
+      caption: "Sign in to see your videos stats",
+    };
+  },
+};
+</script>
