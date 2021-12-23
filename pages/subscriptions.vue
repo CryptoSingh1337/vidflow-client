6a3671d601889cae8bdaf9272c1b8bbb9e5a7c32
Saransh Kumar <cryptosingh@gmail.com>
2021-12-23 19:27:28 +0530

    Closes #8: Created Subscription page.

---
+++ pages/subscriptions.vue
@@ -0,0 +1,30 @@
+<template>
+  <Unauthorize
+    v-if="!$auth.loggedIn"
+    :icon="icon"
+    :heading="heading"
+    :caption="caption"
+  />
+</template>
+
+<script>
+import Unauthorize from "@/components/UnauthorizedUser/Unauthorize.vue";
+
+export default {
+  head() {
+    return {
+      title: "Subscriptions - VidFlow",
+    };
+  },
+  components: {
+    Unauthorize,
+  },
+  data() {
+    return {
+      icon: "mdi-youtube-subscription",
+      heading: "Don't miss new videos",
+      caption: "Sign in to see updates from your favorite channels",
+    };
+  },
+};
+</script>
