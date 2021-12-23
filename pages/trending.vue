0252fa9eb4ec91c79a0bb98656feebeff22ea14a
Saransh Kumar <cryptosingh@gmail.com>
2021-12-23 19:27:54 +0530

    Closes #7: Created Trending page.

---
+++ pages/trending.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container fluid>
+    <h3>Trending</h3>
+    <v-row class="ma-3 mt-1" no-gutters>
+      <v-col
+        :key="video.id"
+        v-for="video in videos"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        class="mx-xs-auto"
+      >
+        <VideoCard :video="video" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import VideoCard from "@/components/Home/VideoCard.vue";
+import data from "@/assets/data/videos.js";
+
+export default {
+  head() {
+    return {
+      title: "Trending - VidFlow",
+    };
+  },
+  components: {
+    VideoCard,
+  },
+  data() {
+    return {
+      videos: data,
+    };
+  },
+};
+</script>
