1926f7cf318ffe1f2f329811814b5bfc67494ffd
Saransh Kumar <cryptosingh@gmail.com>
2021-12-23 19:25:33 +0530

    Closes #4: Created History page.

---
+++ pages/history.vue
@@ -0,0 +1,30 @@
+<template>
+  <Unauthorize
+    v-if="!$auth.loggedIn"
+    :icon="icon"
+    :heading="heading"
+    :caption="caption"
+  />
+</template>
+
+<script>
+import Unauthorize from "@/components/UnauthorizedUser/Unauthorize.vue";
+
+export default {
+  head() {
+    return {
+      title: "History - VidFlow",
+    };
+  },
+  components: {
+    Unauthorize,
+  },
+  data() {
+    return {
+      icon: "mdi-history",
+      heading: "Keep track of what you watch",
+      caption: "Watch history isn't viewable when signed out.",
+    };
+  },
+};
+</script>
