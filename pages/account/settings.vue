74f539dab1a633a7736e3a5ed4a4ebc69d7093ab
Saransh Kumar <cryptosingh@gmail.com>
2021-12-25 00:13:08 +0530

    Closes #22: Created Settings page.

---
+++ pages/account/settings.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>Settings page</h3>
+</template>
+
+<script>
+export default {
+  middleware: "auth",
+  head() {
+    return {
+      title: "Settings - VidFlow",
+    };
+  },
+};
+</script>
