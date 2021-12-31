709edaf49c43c5c8c66847752f6c922fd3221bdc
Saransh Kumar <cryptosingh@gmail.com>
2021-12-31 14:07:45 +0530

    Updated System bar position.

--- components/SystemBar.vue
+++ components/SystemBar.vue
@@ -1,13 +1,15 @@
 <template>
-  <v-system-bar :app="banner ? true : false" height="36" absolute fixed>
+  <v-system-bar :app="banner ? true : false" height="36" fixed>
     <v-spacer></v-spacer>
-    The backend of this application is hosted on Heroku (free dynos). So it
-    might take 1-2 mins on first visit. Check out&nbsp;
-    <a
-      href="https://devcenter.heroku.com/articles/free-dyno-hours"
-      target="_blank"
-      >official article</a
-    >.
+    <div :class="[$vuetify.breakpoint.smAndDown ? 'xs' : 'md', 'text-center']">
+      The backend of this application is hosted on Heroku (free dynos). So it
+      might take 1-2 mins on first visit. Check out&nbsp;
+      <a
+        href="https://devcenter.heroku.com/articles/free-dyno-hours"
+        target="_blank"
+        >official article</a
+      >.
+    </div>
     <v-spacer></v-spacer>
     <v-btn @click.prevent="banner = false" icon
       ><v-icon>mdi-close</v-icon></v-btn
@@ -24,3 +26,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.xs {
+  font-size: 9px;
+}
+.md {
+  font-size: 13.28px;
+}
+</style>
