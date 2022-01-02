37dee68b699ba6c3533a44b99d722fe6b9607174
Saransh Kumar <cryptosingh@gmail.com>
2022-01-02 18:38:46 +0530

    Wrap around in client-only tag

--- pages/history.vue
+++ pages/history.vue
@@ -1,37 +1,39 @@
 <template>
-  <Unauthorize
-    v-if="!$auth.loggedIn"
-    :icon="icon"
-    :heading="heading"
-    :caption="caption"
-  />
-  <v-container v-else-if="$auth.loggedIn && history.length > 0" fluid>
-    <v-row align="center" justify="center" no-gutters>
-      <v-col cols="12" md="10" lg="8">
-        <client-only>
-          <SearchResultCard
-            :key="i"
-            v-for="(video, i) in history"
-            :video="video"
-          />
-        </client-only>
-      </v-col>
-    </v-row>
-  </v-container>
-  <v-container
-    v-else
-    class="d-flex mt-16 pt-16 align-center justify-center flex-column"
-  >
-    <v-icon
-      class="mb-5"
-      :color="$vuetify.theme.dark ? '' : '#000000'"
-      :size="$vuetify.breakpoint.mdAndUp ? 115 : 100"
-      >mdi-information</v-icon
+  <client-only>
+    <Unauthorize
+      v-if="!$auth.loggedIn"
+      :icon="icon"
+      :heading="heading"
+      :caption="caption"
+    />
+    <v-container v-else-if="$auth.loggedIn && history.length > 0" fluid>
+      <v-row align="center" justify="center" no-gutters>
+        <v-col cols="12" md="10" lg="8">
+          <client-only>
+            <SearchResultCard
+              :key="i"
+              v-for="(video, i) in history"
+              :video="video"
+            />
+          </client-only>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container
+      v-else
+      class="d-flex mt-16 pt-16 align-center justify-center flex-column"
     >
-    <div class="mx-auto text-h3 text-center font-weight-bold">
-      Your watch history is empty!
-    </div>
-  </v-container>
+      <v-icon
+        class="mb-5"
+        :color="$vuetify.theme.dark ? '' : '#000000'"
+        :size="$vuetify.breakpoint.mdAndUp ? 115 : 100"
+        >mdi-information</v-icon
+      >
+      <div class="mx-auto text-h3 text-center font-weight-bold">
+        Your watch history is empty!
+      </div>
+    </v-container>
+  </client-only>
 </template>
 
 <script>
