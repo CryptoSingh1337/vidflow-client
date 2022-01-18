9fddc966011ecbce7e2aedab6b096572dffe9a89
Saransh Kumar <cryptosingh@gmail.com>
2022-01-19 00:52:50 +0530

    Created videos page

---
+++ components/Video/VideoCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-card
+    class="ma-2 rounded-0"
+    :color="$vuetify.theme.dark ? '#121212' : ''"
+    :to="`/watch/${video.id}`"
+    flat
+    nuxt
+    :ripple="false"
+  >
+    <v-img
+      :src="video.thumbnail"
+      alt="thumbnail"
+      :aspect-ratio="16 / 9"
+    ></v-img>
+    <div
+      class="my-4 subtitle-1 font-weight-bold"
+      v-text="`${video.title.substring(0, 60)}...`"
+      style="line-height: normal"
+    ></div>
+    <v-divider></v-divider>
+    <v-card-title>Statistics</v-card-title>
+    <v-card-text>
+      <div class="d-flex justify-space-between">
+        <span>Visibility</span>
+        <span
+          ><v-icon
+            class="mr-2"
+            :color="video.videoStatus === 'PUBLIC' ? 'green' : 'grey'"
+            >mdi-eye</v-icon
+          >
+          {{ video.videoStatus | capitalize }}</span
+        >
+      </div>
+      <div class="d-flex justify-space-between">
+        <span>Date</span>
+        <span>{{ $moment(video.createdAt).format("D, MMM YYYY") }}</span>
+      </div>
+      <div class="d-flex justify-space-between">
+        <span>Views</span>
+        <span>{{ video.views | numberfy }}</span>
+      </div>
+      <div class="d-flex justify-space-between mt-2">
+        <span>Likes (vs. Dislikes)</span>
+        <span>
+          <div style="text-align: end">
+            {{ `${calculateRatio(video.likes, video.dislikes)}%` }}
+          </div>
+          <div>{{ video.likes | numberfy }} likes</div>
+          <v-progress-linear
+            class="my-2"
+            color="grey"
+            :value="calculateRatio(video.likes, video.dislikes)"
+            rounded
+          ></v-progress-linear>
+        </span>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    video: Object,
+  },
+  methods: {
+    calculateRatio(likes, dislikes) {
+      return ((likes / (likes + dislikes)) * 100).toFixed(2);
+    },
+  },
+  filters: {
+    capitalize: (text) => {
+      return `${text.substring(0, 1)}${text.substring(1).toLowerCase()}`;
+    },
+    numberfy: (views) => views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+  },
+};
+</script>
--- pages/videos.vue
+++ pages/videos.vue
@@ -5,26 +5,73 @@
     :heading="heading"
     :caption="caption"
   />
+  <Loader v-else-if="$fetchState.pending" />
+  <v-container
+    class="fill-height justify-center"
+    v-else-if="videos.length === 0"
+  >
+    <h3 class="mt-5 text-md-h5 font-weight-medium">No videos found</h3>
+  </v-container>
+  <v-container v-else-if="$vuetify.breakpoint.mdAndUp">
+    <h3 class="text-h5 font-weight-bold mb-5">Your Videos</h3>
+    <VideoList :videos="videos" />
+    <div class="text-center mt-5">
+      <v-pagination v-model="page" :length="6"></v-pagination>
+    </div>
+  </v-container>
+  <v-container v-else>
+    <v-row no-gutters>
+      <v-col cols="12" sm="6" :key="i" v-for="(video, i) in videos">
+        <VideoCard :video="video" />
+      </v-col>
+    </v-row>
+    <div class="text-center mt-5">
+      <v-pagination v-model="page" :length="6"></v-pagination>
+    </div>
+  </v-container>
 </template>
 
 <script>
 import Unauthorize from "@/components/UnauthorizedUser/Unauthorize.vue";
+import Loader from "@/components/Loader.vue";
+import VideoList from "@/components/Video/VideoList.vue";
 
 export default {
   head() {
     return {
-      title: "Your vidoes - VidFlow",
+      title: "Your videos - VidFlow",
     };
   },
   components: {
     Unauthorize,
+    Loader,
+    VideoList,
   },
   data() {
     return {
       icon: "mdi-video",
       heading: "Keep your eyes on your vidoes stats",
       caption: "Sign in to see your videos stats",
+      page: 1,
+      videos: [],
     };
+  },
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+    calculateRatio(likes, dislikes) {
+      return ((likes / (likes + dislikes)) * 100).toFixed(2);
+    },
+  },
+  async fetch() {
+    if (this.$auth.loggedIn) {
+      const response = await this.$axios.get(
+        `/video/user/${this.$auth.user.id}/all?page=${this.page}`
+      );
+      const videos = await response.data;
+      this.videos = videos;
+    }
   },
 };
 </script>
