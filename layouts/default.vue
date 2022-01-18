ba24873c633dbdc8f5ea384c037e662b36a3894f
Saransh Kumar <cryptosingh@gmail.com>
2022-01-19 00:51:19 +0530

    Lowered the scrollbar width

--- layouts/default.vue
+++ layouts/default.vue
@@ -51,11 +51,11 @@ export default {
   scroll-behavior: smooth;
 }
 *::-webkit-scrollbar {
-  width: 10px;
+  width: 5px;
 }
 *::-webkit-scrollbar-thumb {
   background-color: #d3d3d3;
-  border-radius: 20px;
+  border-radius: 10px;
   border: 1px solid #c5c5c5;
 }
 </style>
