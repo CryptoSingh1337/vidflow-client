de872d1f5bd0b1fd42a12a83fe293c5f7256e8c0
Saransh Kumar <cryptosingh@gmail.com>
2022-01-19 00:50:18 +0530

    Created Loader component

---
+++ components/Loader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container fill-height justify-center flex-column">
+    <v-progress-circular
+      :size="100"
+      :width="7"
+      color="#1867c0"
+      indeterminate
+    ></v-progress-circular>
+    <h3 class="mt-5 text-md-h5 font-weight-medium">Loading...</h3>
+  </div>
+</template>
