733dd67a48c97e1341375bc8995d90b8a41fc953
Saransh Kumar <cryptosingh@gmail.com>
2023-01-17 16:24:28 +0530

    Remove custom theme and used md3 design

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,30 +1,11 @@
-import { createVuetify, ThemeDefinition } from 'vuetify'
+import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'
+import { md3 } from 'vuetify/blueprints'
 import '@mdi/font/css/materialdesignicons.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const customTheme: ThemeDefinition = {
-    dark: false,
-    colors: {
-      background: '#FFFFFF',
-      surface: '#FFFFFF',
-      primary: '#1976D2',
-      secondary: '#424242',
-      accent: '#82B1FF',
-      error: '#FF5252',
-      info: '#2196F3',
-      success: '#4CAF50',
-      warning: '#FFC107'
-    }
-  }
-
   const vuetify = createVuetify({
-    theme: {
-      defaultTheme: 'customTheme',
-      themes: {
-        customTheme
-      }
-    },
+    blueprint: md3,
     icons: {
       defaultSet: 'mdi',
       aliases,
