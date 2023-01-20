715fdc3f7faf15183368b895bfbc1c17141b400f
Saransh Kumar <cryptosingh@gmail.com>
2023-01-21 00:32:45 +0530

    Create global alert snackbar

---
+++ components/GlobalAlert.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-snackbar
+    :model-value="model"
+    :color="type"
+    location="top"
+    timeout="2000"
+    transition="scale-transition"
+  >
+    <v-icon icon="icon" />
+    <div class="Subtitle-2 d-inline">
+      {{ text }}
+    </div>
+  </v-snackbar>
+</template>
+
+<script lang='ts' setup>
+defineProps<{
+    model: boolean,
+    type: string,
+    text: string
+}>()
+</script>
