42ff883df77a13db1ab67765431babc4b3044334
Saransh Kumar <cryptosingh@gmail.com>
2023-01-22 15:59:29 +0530

    Create global page loader

---
+++ components/GlobalPageLoader.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout full-height class="flex-column align-center justify-center">
+    <v-progress-circular
+      :size="100"
+      :width="7"
+      color="#1867c0"
+      indeterminate
+    />
+    <h3 class="mt-5 text-md-h5 font-weight-medium">
+      Loading...
+    </h3>
+  </v-layout>
+</template>
