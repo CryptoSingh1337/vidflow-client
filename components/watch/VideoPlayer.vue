dd092152e76053817317dc7285b639b44d72dcec
Saransh Kumar <cryptosingh@gmail.com>
2023-01-22 15:59:09 +0530

    Create video player

---
+++ components/watch/VideoPlayer.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="d-flex justify-center bg-black">
+    <div ref="player" class="player mx-auto" style="width: 60%; height: 50%;" />
+  </div>
+</template>
+
+<script lang='ts' setup>
+import ArtPlayer from 'artplayer'
+
+const player = ref(null)
+const artPlayer = ref(null)
+
+const props = defineProps<{
+  src: string,
+  title: string,
+  thumbnail: string
+}>()
+
+const options = reactive({
+  url: props.src,
+  title: props.title,
+  poster: props.thumbnail,
+  volume: 0.5,
+  isLive: false,
+  muted: false,
+  autoplay: false,
+  pip: true,
+  setting: true,
+  loop: false,
+  playbackRate: true,
+  aspectRatio: true,
+  fullscreen: true,
+  fullscreenWeb: true,
+  miniProgressBar: true,
+  mutex: true,
+  backdrop: true,
+  playsInline: true,
+  airplay: true,
+  whitelist: ['*']
+})
+
+onMounted(() => {
+  nextTick(() => {
+    artPlayer.value = new ArtPlayer({
+      ...options,
+      container: '.player'
+    })
+  })
+})
+
+onBeforeUnmount(() => {
+  if (artPlayer.value && artPlayer.value.destroy) {
+    artPlayer.value.destroy(false)
+  }
+})
+
+defineExpose({
+  player,
+  options
+})
+</script>
+
+<style>
+.player {
+    aspect-ratio: 16/9;
+}
+</style>
--- pages/watch/[id].vue
+++ pages/watch/[id].vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
+  <GlobalPageLoader v-if="pending" />
+  <div v-else>
+    <WatchVideoPlayer :src="video.videoUrl" :title="video.title" :thumbnail="video.thumbnail" />
     {{ route.params.id }}
+    <div>
+      Video:
+      {{ video }}
+    </div>
+    <div>
+      Trending video:
+      {{ trendingVideo }}
+    </div>
   </div>
 </template>
 
 <script lang='ts' setup>
 const route = useRoute()
+const page = ref(0)
 
 definePageMeta({ auth: false })
+
+const { pending, data: video } = await useFetch(`/api/video/${route.params.id}`, {
+  key: route.params.id as string
+})
+
+const { data: trendingVideo } = await useFetch(`/api/video/trending?page=${page.value}`)
 </script>
