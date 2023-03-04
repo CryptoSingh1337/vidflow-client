a43d41efd3bed452308b8b8002b5f59915210d82
Saransh Kumar <cryptosingh@gmail.com>
2023-03-04 16:55:36 +0530

    Fix #41 and modify the channel and avatar div

--- components/nav/AvatarButton.vue
+++ components/nav/AvatarButton.vue
@@ -1,28 +1,26 @@
 <template>
-  <v-menu location="bottom">
+  <v-menu location="bottom" :close-on-content-click="false">
     <template #activator="{ props }">
       <v-avatar class="btn mr-3" v-bind="props">
         <v-img :src="user.profileImage" />
       </v-avatar>
     </template>
-    <v-list density="compact" class="pt-0">
-      <v-row class="mt-1 mb-1 mx-0">
-        <v-col cols="3" class="pa-auto">
-          <v-avatar>
+    <v-list density="compact">
+      <v-list-item>
+        <template #prepend>
+          <v-avatar size="40">
             <v-img :src="user.profileImage" />
           </v-avatar>
-        </v-col>
-        <v-col class="pa-0">
-          <h3>{{ user.username }}</h3>
-          <NuxtLink to="/account/settings">
-            <span class="text-caption">Manage your account</span>
-          </NuxtLink>
-        </v-col>
-      </v-row>
-      <v-divider class="px-2" />
-      <v-list-item rounded="xl" :to="`/channel/${user.id}`" prepend-icon="mdi-account" title="Your Channel" />
+        </template>
+        <h3>{{ user.username }}</h3>
+        <NuxtLink to="/account/settings">
+          <span class="text-caption">Manage your account</span>
+        </NuxtLink>
+      </v-list-item>
+      <v-divider class="mx-2" />
+      <v-list-item rounded="xl" :to="`/channel/${user.id}`" prepend-icon="mdi-account" title="Your channel" />
       <v-list-item rounded="xl" prepend-icon="mdi-logout" title="Sign out" @click="$auth.signOut({ redirect: false })" />
-      <v-divider class="px-2" />
+      <v-divider class="mx-2" />
       <NavThemeChanger />
       <NavInfoSection />
     </v-list>
